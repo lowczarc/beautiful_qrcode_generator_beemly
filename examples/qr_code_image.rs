ebbbe7ee3f90a4e16a9ca900eef52e27152ea545
Lancelot OWCZARCZAK <lowczarc@student.42.fr>
2019-04-25 15:13:50 +0200

    Add CARGO_DIR

--- examples/qr_code_image.rs
+++ examples/qr_code_image.rs
@@ -1,4 +1,4 @@
-use rust_design_qrcode::*;
+use beautiful_qrcode_generator_beemly::*;
 
 fn main() {
     qrcode_generate("Hello World !").save("target/qrcode.png");
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,8 @@ use std::path::Path;
 use qrcode_generator::QrCodeEcc;
 use image::{DynamicImage, ImageBuffer, open, Rgb};
 
+const CARGO_DIR : &str = concat!(env!("CARGO_MANIFEST_DIR"), "/");
+
 pub type ImageBuff8 = ImageBuffer<Rgb<u8>, Vec<u8>>;
 
 fn get_block(bits: [bool; 4], block_table: &[DynamicImage; 4]) -> DynamicImage {
@@ -53,13 +55,13 @@ pub fn qrcode_generate(message: &str) -> ImageBuff8 {
     let qrcode_matrix = qrcode_matrix(message);
     let mut qrcode_image = ImageBuff8::from_fn(qrcode_matrix[0].len() as u32 * 35, qrcode_matrix.len() as u32 * 35, |x: u32, y: u32| { Rgb{ data: [255, 255, 255] }});
 
-    let eye = open(Path::new("assets/eye.png")).unwrap();
-    let image_center = open(Path::new("assets/image_center.png")).unwrap();
+    let eye = open(Path::new(&format!("{}assets/eye.png", CARGO_DIR))).unwrap();
+    let image_center = open(Path::new(&format!("{}assets/image_center.png", CARGO_DIR))).unwrap();
     let block_table: [DynamicImage; 4] = [
-        open(Path::new("assets/0.png")).unwrap(),
-        open(Path::new("assets/1.png")).unwrap(),
-        open(Path::new("assets/2.png")).unwrap(),
-        open(Path::new("assets/3.png")).unwrap(),
+        open(Path::new(&format!("{}assets/0.png", CARGO_DIR))).unwrap(),
+        open(Path::new(&format!("{}assets/1.png", CARGO_DIR))).unwrap(),
+        open(Path::new(&format!("{}assets/2.png", CARGO_DIR))).unwrap(),
+        open(Path::new(&format!("{}assets/3.png", CARGO_DIR))).unwrap(),
     ];
 
     for line_id in 0..qrcode_matrix.len() {
