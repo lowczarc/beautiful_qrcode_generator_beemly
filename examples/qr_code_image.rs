b5bf426baaf759c65b245351627aaa6c59412664
Lancelot OWCZARCZAK <lowczarc@student.42.fr>
2019-06-01 15:46:00 +0200

    rustfmt

--- examples/qr_code_image.rs
+++ examples/qr_code_image.rs
@@ -2,6 +2,10 @@ use beautiful_qrcode_generator_beemly::*;
 
 fn main() {
     qrcode_generate("B").save("target/qrcode0.png").unwrap();
-    qrcode_generate("https://beemly/").save("target/qrcode1.png").unwrap();
-    qrcode_generate("Ceci est un message plus long").save("target/qrcode2.png").unwrap();
+    qrcode_generate("https://beemly/")
+        .save("target/qrcode1.png")
+        .unwrap();
+    qrcode_generate("Ceci est un message plus long")
+        .save("target/qrcode2.png")
+        .unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 use std::path::Path;
 
+use image::{open, DynamicImage, ImageBuffer, Rgb};
 use qrcode_generator::QrCodeEcc;
-use image::{DynamicImage, ImageBuffer, open, Rgb};
 
-const CARGO_DIR : &str = concat!(env!("CARGO_MANIFEST_DIR"), "/");
+const CARGO_DIR: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/");
 
 pub type ImageBuff8 = ImageBuffer<Rgb<u8>, Vec<u8>>;
 
@@ -11,7 +11,7 @@ fn get_block(bits: [bool; 4], block_table: &[DynamicImage; 4]) -> DynamicImage {
     let (mode, rotation) = {
         let mut mode = 0;
         let mut rotation = 0;
-        for i in 0..4{
+        for i in 0..4 {
             if bits[i] && mode == 0 {
                 mode = 1;
                 rotation = i;
@@ -37,28 +37,33 @@ fn get_block(bits: [bool; 4], block_table: &[DynamicImage; 4]) -> DynamicImage {
 }
 
 fn copy_image_into_another(dest: &mut ImageBuff8, src: &ImageBuff8, x: u32, y: u32) {
-   if y + src.height() > dest.height() || x + src.width() > dest.width() {
-       panic!("dest overflow in copy_image_into_another");
-   }
-   for i in 0..src.height() {
-       for j in 0..src.width() {
+    if y + src.height() > dest.height() || x + src.width() > dest.width() {
+        panic!("dest overflow in copy_image_into_another");
+    }
+    for i in 0..src.height() {
+        for j in 0..src.width() {
             dest.put_pixel(j + x, i + y, src.get_pixel(j, i).clone());
-       }
-   }
+        }
+    }
 }
 
 pub fn qrcode_matrix(message: &str) -> Vec<Vec<bool>> {
-    let mut qrcode_matrix = qrcode_generator::to_matrix(message, QrCodeEcc::High).expect("Unable to create QR Code for this message");
+    let mut qrcode_matrix = qrcode_generator::to_matrix(message, QrCodeEcc::High)
+        .expect("Unable to create QR Code for this message");
 
     let draw_emoji = (9. * 9. / (qrcode_matrix.len() * qrcode_matrix[0].len()) as f32) < 0.15;
-    
+
     for line_id in 0..qrcode_matrix.len() {
         for block_id in 0..qrcode_matrix[line_id].len() {
             if (line_id < 7 && block_id < 7)
                 || (line_id >= qrcode_matrix.len() - 7 && block_id < 7)
                 || (line_id < 7 && block_id >= qrcode_matrix[line_id].len() - 7)
-                || (draw_emoji && line_id <= (qrcode_matrix.len() / 2) + 4 && line_id >= (qrcode_matrix.len() / 2) - 4
-                    && block_id <= (qrcode_matrix[line_id].len() / 2) + 4 && block_id >= (qrcode_matrix[line_id].len() / 2) - 4) {
+                || (draw_emoji
+                    && line_id <= (qrcode_matrix.len() / 2) + 4
+                    && line_id >= (qrcode_matrix.len() / 2) - 4
+                    && block_id <= (qrcode_matrix[line_id].len() / 2) + 4
+                    && block_id >= (qrcode_matrix[line_id].len() / 2) - 4)
+            {
                 qrcode_matrix[line_id][block_id] = false;
             }
         }
@@ -68,8 +73,13 @@ pub fn qrcode_matrix(message: &str) -> Vec<Vec<bool>> {
 
 pub fn qrcode_generate(message: &str) -> ImageBuff8 {
     let qrcode_matrix = qrcode_matrix(message);
-    let mut qrcode_image = ImageBuff8::from_fn(qrcode_matrix[0].len() as u32 * 35, qrcode_matrix.len() as u32 * 35, |_x: u32, _y: u32| { Rgb { data: [255, 255, 255] } });
-
+    let mut qrcode_image = ImageBuff8::from_fn(
+        qrcode_matrix[0].len() as u32 * 35,
+        qrcode_matrix.len() as u32 * 35,
+        |_x: u32, _y: u32| Rgb {
+            data: [255, 255, 255],
+        },
+    );
 
     let eye = open(Path::new(&format!("{}assets/eye.png", CARGO_DIR))).unwrap();
     let image_center = open(Path::new(&format!("{}assets/image_center.png", CARGO_DIR))).unwrap();
@@ -85,21 +95,42 @@ pub fn qrcode_generate(message: &str) -> ImageBuff8 {
             if qrcode_matrix[line_id][block_id] {
                 let bits = [
                     line_id != 0 && qrcode_matrix[line_id - 1][block_id],
-                    block_id != qrcode_matrix[line_id].len() - 1 && qrcode_matrix[line_id][block_id + 1],
+                    block_id != qrcode_matrix[line_id].len() - 1
+                        && qrcode_matrix[line_id][block_id + 1],
                     line_id != qrcode_matrix.len() - 1 && qrcode_matrix[line_id + 1][block_id],
                     block_id != 0 && qrcode_matrix[line_id][block_id - 1],
                 ];
-                copy_image_into_another(&mut qrcode_image, &get_block(bits, &block_table).to_rgb(), block_id as u32 * 35, line_id as u32 * 35);
+                copy_image_into_another(
+                    &mut qrcode_image,
+                    &get_block(bits, &block_table).to_rgb(),
+                    block_id as u32 * 35,
+                    line_id as u32 * 35,
+                );
             }
         }
     }
 
     copy_image_into_another(&mut qrcode_image, &eye.to_rgb(), 0, 0);
-    copy_image_into_another(&mut qrcode_image, &eye.rotate270().to_rgb(), 0, (qrcode_matrix.len() as u32 - 7) * 35);
-    copy_image_into_another(&mut qrcode_image, &eye.rotate90().to_rgb(), (qrcode_matrix[0].len() as u32 - 7) * 35, 0);
-    
+    copy_image_into_another(
+        &mut qrcode_image,
+        &eye.rotate270().to_rgb(),
+        0,
+        (qrcode_matrix.len() as u32 - 7) * 35,
+    );
+    copy_image_into_another(
+        &mut qrcode_image,
+        &eye.rotate90().to_rgb(),
+        (qrcode_matrix[0].len() as u32 - 7) * 35,
+        0,
+    );
+
     if (9. * 9. / (qrcode_matrix.len() * qrcode_matrix[0].len()) as f32) < 0.15 {
-        copy_image_into_another(&mut qrcode_image, &image_center.to_rgb(), (qrcode_matrix.len() as u32 / 2 - 4) * 35, (qrcode_matrix[0].len() as u32 / 2 - 4) * 35);
+        copy_image_into_another(
+            &mut qrcode_image,
+            &image_center.to_rgb(),
+            (qrcode_matrix.len() as u32 / 2 - 4) * 35,
+            (qrcode_matrix[0].len() as u32 / 2 - 4) * 35,
+        );
     }
 
     qrcode_image
